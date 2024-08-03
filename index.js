6f7e2dd62a183b1f70539d958c2efd96550af092
Ozelot <contact@ozelot.ovh>
2024-08-03 20:22:55 +0200

    App functionality outline

--- index.js
+++ index.js
@@ -1,1 +1,129 @@
-console.log("init")
+import { fetchers } from "proxy-master";
+import { ProxyAgent } from "undici";
+
+import config from "./config.json" assert { type: "json" };
+
+import { setTimeout as wait } from "node:timers/promises";
+import { readFile, writeFile } from "fs/promises";
+
+(async () => {
+
+    console.log("Loading proxies...");
+
+    const fetcher = fetchers.combine({
+        fetchers: [      
+            fetchers.file({ path: "./proxies.txt" }),
+        ]
+    });
+      
+    await fetcher.fetch();
+
+    fetcher.refetchOnInterval(10_000);
+
+    let ratelimit = 0;
+
+    const available = JSON.parse(await readFile("./data/available.json"));
+    const unavailable = JSON.parse(await readFile("./data/unavailable.json"));
+
+    console.log(`Loaded ${available.length} available and ${unavailable.length} unavailable saved clans.`);
+
+    const res = await fetch("https://discord.com/api/v9/discovery/games/all", {
+        headers: {
+            "Authorization": config.token
+        }
+    });
+
+    const body = await res.json();
+    let clans = body.clans;
+
+    if(!clans) return console.error("Could not found any clans!", body);
+
+    console.log(`Got ${clans.length} clans...`);
+
+    if(config.max_members) {
+        clans = clans.filter(item => (item.member_count <= config.max_members));
+        console.log(`Got ${clans.length} clans... (clans up to ${config.max_members} members)`);
+    }
+
+    clans = clans.filter(item => !unavailable.includes(item.id));
+    console.log(`Got ${clans.length} clans... (ignored already checked clans)`);
+
+    console.log("Starting checking...");
+
+    setInterval(async () => {
+        await writeFile("./data/unavailable.json", JSON.stringify(unavailable, null, 2));
+        console.log("Results have been saved.")
+    }, config.save_interval * 1000)
+
+    for(const clan of clans) {
+
+        await wait(config.cooldown * 1000);
+
+        const proxy = fetcher.random();
+        const displayProxy = `${proxy[0].host}:${proxy[0].port}`;
+
+        try {
+
+            const clanData = await fetch(`https://discord.com/api/guilds/${clan.id}/widget.json`, {
+                dispatcher: new ProxyAgent(`http://${displayProxy}`)
+            });
+    
+            if(clanData.status == 403) {
+                console.log(`[${clan.id}] [${clan.tag}] [${displayProxy}] The widget feature is disabled`);
+                unavailable.push(clan.id);
+                continue;
+            }
+
+            else if(clanData.status == 429) {
+                ratelimit++;
+                console.log(`[${clan.id}] [${clan.tag}] [${displayProxy}] Too many requests (${ratelimit}/${config.max_ratelimit_attempts})`);
+                if(ratelimit >= config.max_ratelimit_attempts) return process.exit();
+                continue;
+            }
+    
+            else if(clanData.status !== 200) {
+                console.log(`[${clan.id}] [${clan.tag}] [${displayProxy}] Could not fetch widget data - ${clanData.status}`, clanData);
+                continue;
+            };
+
+            if(ratelimit > 0) ratelimit = 0;
+    
+            const clanBody = await clanData.json();
+    
+            if(!clanBody.instant_invite) {
+                console.log(`[${clan.id}] [${clan.tag}] [${displayProxy}] Could not found public invite`);
+                unavailable.push(clan.id);
+                continue;
+            }
+    
+            if(available.find(item => item.invite == clanBody.instant_invite)) { // Checking this here because if new invite to previously sent clan appear, we can handle it
+                console.log(`[${clan.id}] [${clan.tag}] [${displayProxy}] Already saved as available`);
+                continue;
+            }
+    
+            available.push({ id: clan.id, name: clan.name, tag: clan.tag, invite: clanBody.instant_invite, foundAt: Date.now() });
+            await writeFile("./data/available.json", JSON.stringify(available, null, 2));
+    
+            await fetch(config.webhook, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    content: `# [${clan.tag}] ${clan.name}\n\n_${clan.description.slice(0, 1600)}_\n\n${clanBody.instant_invite}\n\n-# ${clan.id}`
+                })
+            })
+    
+            console.log(`[${clan.id}] [${clan.tag}] [${displayProxy}] Found new clan [${clan.name}] - ${clanBody.instant_invite}`);
+
+        } catch(err) {
+            console.log(`[${clan.id}] [${clan.tag}] [${displayProxy}] An unknown error occured:`, err.message);
+        }
+
+    }
+
+    await writeFile("./data/unavailable.json", JSON.stringify(unavailable, null, 2));
+
+    process.exit();
+
+})();
